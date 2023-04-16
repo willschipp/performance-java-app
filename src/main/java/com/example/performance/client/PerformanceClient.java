ca70613f11983315a92f37e559a222f2abb9000e
Will Schipp <3287360+willschipp@users.noreply.github.com>
2023-04-15 19:47:16 -0500

    WIP: minor fixes

--- src/main/java/com/example/performance/client/PerformanceClient.java
+++ src/main/java/com/example/performance/client/PerformanceClient.java
@@ -1,10 +1,12 @@
 package com.example.performance.client;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import lombok.extern.slf4j.Slf4j;
@@ -18,12 +20,26 @@ public class PerformanceClient {
     @Autowired
     WebClient webClient;
 
+    @Bean
+    public WebClient webClient() {
+        final int size = 16 * 1024 * 1024;
+        final ExchangeStrategies strategies = ExchangeStrategies.builder()
+            .codecs(codecs -> codecs.defaultCodecs().maxInMemorySize(size))
+            .build();
+        return WebClient.builder()
+            .exchangeStrategies(strategies)
+            .build();
+    }
+
     public void sendRequest() throws Exception {
         log.info("enter");
         //use a webclient to send a request
-        webClient.get()
+        String body = webClient.get()
             .uri("/news")
-            .exchangeToMono(response -> {});
+            .exchangeToMono(handler -> {
+                return handler.bodyToMono(String.class);
+            }).block();
+        log.info(body);
         // });
         log.info("exit");
     }
--- src/test/java/com/example/performance/client/PerformanceClientIT.java
+++ src/test/java/com/example/performance/client/PerformanceClientIT.java
@@ -1,22 +1,38 @@
 package com.example.performance.client;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
 public class PerformanceClientIT {
     
+    WebClient webClient;
+
+    @BeforeEach
+    public void setup() throws Exception {
+        final int size = 16 * 1024 * 1024;
+        final ExchangeStrategies strategies = ExchangeStrategies.builder()
+            .codecs(codecs -> codecs.defaultCodecs().maxInMemorySize(size))
+            .build();        
+        webClient = WebClient.builder()
+        .exchangeStrategies(strategies)
+        .baseUrl("https://www.bbc.com/")
+        .build();            
+    }
+
 
     @Test
     public void test_sendRequest() throws Exception {
-        WebClient webClient = WebClient.create("https://www.bbc.com/");
+        // WebClient webClient = WebClient.create("https://www.bbc.com/");
         //instantiate
         PerformanceClient performanceClient = new PerformanceClient();
         ReflectionTestUtils.setField(performanceClient, "webClient",webClient);
         //invoke
         performanceClient.sendRequest();
         //wait
-        Thread.sleep(10 * 1000);
+        // Thread.sleep(10 * 1000);
 
     }
 }
