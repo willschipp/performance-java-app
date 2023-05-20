682d1b30cc9066e36e269fdd9473f92a0c7c0c5f
Will Schipp <3287360+willschipp@users.noreply.github.com>
2023-05-20 06:43:00 -0500

    gatling details and fixes

--- src/gatling/java/com/example/performance/gatling/SimpleLoadSimulation.java
+++ src/gatling/java/com/example/performance/gatling/SimpleLoadSimulation.java
@@ -6,23 +6,27 @@ import io.gatling.javaapi.http.*;
 import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.*;
 
-public class SimpleLoad extends Simulation {
+public class SimpleLoadSimulation extends Simulation {
 
-  HttpProtocolBuilder httpProtocol = http.baseUrl("http://performance.com")
+
+  HttpProtocolBuilder httpProtocol = http.baseUrl("http://localhost:8080")
       .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
       .doNotTrackHeader("1")
       .acceptLanguageHeader("en-US,en;q=0.5")
       .acceptEncodingHeader("gzip, deflate")
       .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0");
 
-  ScenarioBuilder scn = scenario("BasicSimulation").exec(http("request_1")
-          .get("/persons"))
+      
+  ScenarioBuilder scn = scenario("Persons")
+      .exec(http("request_1")
+        .get("/persons"))
       .pause(5);
 
+              // scn.injectOpen(atOnceUsers(1))).protocols(httpProtocol);
+
   {
-    setUp( // 11
-        scn.injectOpen(atOnceUsers(1))
-    ).protocols(httpProtocol);
+    setUp(
+        scn.injectOpen(rampUsers(10).during(10))).protocols(httpProtocol);
   }
 
 }
